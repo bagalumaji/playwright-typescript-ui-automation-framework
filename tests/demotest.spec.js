20cc1bd292e3133ecce9dea8c7fb3ef5d3529783
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-05-31 19:44:37 +0530

    added demo test

---
+++ tests/demotest.spec.js
@@ -0,0 +1,8 @@
+import {test,expect} from "@playwright/test";
+
+test("demo test",async ({page})=>{
+    await page.goto('http://saucelabs.com/');
+    console.log(await page.title());
+    await  page.waitForTimeout(5000);
+
+})
